3e1bd60554f10817fcdc76107b7ed3291a3d4c02
Klaus-in-Tech <kkallan07@gmail.com>
2023-12-17 20:14:25 +0300

    hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Kakooza Allan Klaus.</h2>;
+  const three = <h3 className="big-heading">I build intelligent automation solutions.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a competent and versatile functional professional, bringing combination of banking
+        industry transformation primarily focusing on RPA, while also covering technologies such as
+        ML, AI, Java and Python. Currently offering my services at{' '}
+        <a href="https://rpapp.io/rpa-services" target="_blank" rel="noreferrer">
+          RPApp.io
         </a>
         .
       </p>
@@ -78,10 +78,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.youtube.com/channel/UCbChxT4XT0MUhySZvTBZ0Mg"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my YouTube!
     </a>
   );
 
