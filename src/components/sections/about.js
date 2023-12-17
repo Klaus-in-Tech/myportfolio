c58100c2b6f9a27dce4dc17f18911570865a26bf
Klaus-in-Tech <kkallan07@gmail.com>
2023-12-17 21:52:22 +0300

    about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'TypeScript', 'React', 'UiPath', 'Robocorp', 'WordPress'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Kakooza and I enjoy creating things that live on. My interest in
+              software development started back in 2016 when I joined computer science at Makerere
+              University.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://witsmind.com/">a start-up</a>, and{' '}
+              <a href="https://www.stanbicbank.co.ug/">the best bank in Uganda</a>, My main focus
+              these days is building intelligent automation solutions at{' '}
+              <a href="https://rpapp.io/rpa-services">RPApp.io</a> for a variety of clients.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
